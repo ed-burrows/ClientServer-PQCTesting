2143d784c4c6f931ad4ca5a08029917b8330fc6e
Ed <s260403@uos.ac.uk>
2025-05-04 17:04:52 +0100

    saveSignature and loadSignature added to FileHandler.java

--- src/main/java/org/example/FileHandler.java
+++ src/main/java/org/example/FileHandler.java
@@ -2,8 +2,11 @@ package org.example;
 
 import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
 
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.Key;
 
 public class FileHandler {
@@ -12,5 +15,15 @@ public class FileHandler {
         try (JcaPEMWriter pemWriter = new JcaPEMWriter(new FileWriter(filepath))){
             pemWriter.writeObject(key);
         }
+    }
+
+    public static void saveSignature(byte[] signature, String filepath) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(filepath)) {
+            fos.write(signature);
+        }
+    }
+
+    public static byte[] loadSignature(String filepath) throws IOException {
+        return Files.readAllBytes(Paths.get(filepath));
     }
 }
