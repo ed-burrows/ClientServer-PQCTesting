d16c93c87a71d5080fa707a52387d846201d388f
Ed <s260403@uos.ac.uk>
2025-05-04 18:27:58 +0100

    Added necessary variables to allow storing filepaths/port for server in ServerApp.java

--- src/main/java/org/example/ServerApp.java
+++ src/main/java/org/example/ServerApp.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import javax.net.ssl.SSLServerSocket;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ServerApp {
+    private static final int PORT = 4444;
+    private static final String MESSAGE_FILE = "receivedfiles/Hello.txt";
+    private static final String SIGNATURE_FILE = "receivedfiles/signature.sig";
+    private static final String PUBLIC_KEY_FILE = "receivedfiles/public_key.pem";
+    private static final Path MESSAGE_FILEPATH = Paths.get(MESSAGE_FILE);
+    private static SSLServerSocket serverSocket;
+    private static boolean serverRunning = false;
+
     public static void main(String[] args) {
+
+    }
+
+    public static void stopServer() {
 
     }
 }
