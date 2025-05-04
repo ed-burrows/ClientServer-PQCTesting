eb08245b7a510844174c3f2acdae2e2743e535c4
Ed <s260403@uos.ac.uk>
2025-05-04 17:00:48 +0100

    savePublicKey added to FileHandler.java

--- src/main/java/org/example/FileHandler.java
+++ src/main/java/org/example/FileHandler.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.Key;
+
 public class FileHandler {
+
+    public static void savePublicKey(Key key, String filepath) throws IOException {
+        try (JcaPEMWriter pemWriter = new JcaPEMWriter(new FileWriter(filepath))){
+            pemWriter.writeObject(key);
+        }
+    }
 }
