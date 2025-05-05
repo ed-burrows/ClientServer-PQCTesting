f11dd019ef09c401c542d4d41e481ec063711f84
Ed <s260403@uos.ac.uk>
2025-05-05 16:05:22 +0100

    'generateDilithiumKeyPair', 'dilithiumSign' and 'dilithiumVerify' created in DilithiumManager.java

--- src/main/java/org/example/DilithiumManager.java
+++ src/main/java/org/example/DilithiumManager.java
@@ -1,4 +1,30 @@
 package org.example;
 
+import org.bouncycastle.pqc.jcajce.spec.DilithiumParameterSpec;
+
+import java.security.*;
+
 public class DilithiumManager {
+    public static KeyPair generateDilithiumKeyPair() throws InvalidAlgorithmParameterException,
+            NoSuchAlgorithmException, NoSuchProviderException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("Dilithium", "BCPQC");
+        keyPairGenerator.initialize(DilithiumParameterSpec.dilithium2);
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    public static byte[] dilithiumSign(byte[] data, PrivateKey privateKey) throws NoSuchAlgorithmException,
+            NoSuchProviderException, InvalidKeyException, SignatureException {
+        Signature signature = Signature.getInstance("Dilithium", "BCPQC");
+        signature.initSign(privateKey);
+        signature.update(data);
+        return signature.sign();
+    }
+
+    public static boolean dilithiumVerify(byte[] data, byte[] signature, PublicKey publicKey) throws NoSuchAlgorithmException,
+            NoSuchProviderException, InvalidKeyException, SignatureException {
+        Signature verifySignature = Signature.getInstance("Dilithium", "BCPQC");
+        verifySignature.initVerify(publicKey);
+        verifySignature.update(data);
+        return verifySignature.verify(signature);
+    }
 }
