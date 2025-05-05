30b25368886848062ea6079105ef039504ab378d
Ed <s260403@uos.ac.uk>
2025-05-05 15:41:56 +0100

    Client functionality added to ClientApp.java. Also added initial version of RSA signature implementation

--- src/main/java/org/example/ClientApp.java
+++ src/main/java/org/example/ClientApp.java
@@ -1,7 +1,89 @@
 package org.example;
 
-public class ClientApp {
-    public static void main(String[] args) {
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.KeyPair;
+import java.security.KeyStore;
+import java.security.Security;
+
+public class ClientApp {
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int PORT = 4444;
+    private static final String MESSAGE_FILE = "files/Hello.txt";
+    private static final String SIGNATURE_FILE = "files/signature.sig";
+    private static final String PUBLIC_KEY_FILE = "files/public_key.pem";
+    private static final String PRIVATE_KEY_FILE = "files/private_key.pem";
+    private static final Path MESSAGE_FILE_PATH = Path.of(MESSAGE_FILE);
+    private static final String[] FILES_TO_SEND = {MESSAGE_FILE, SIGNATURE_FILE, PUBLIC_KEY_FILE};
+
+    public static void main(String[] args) throws Exception {
+        Security.addProvider(new BouncyCastleProvider());
+
+        KeyPair keyPair = RSAManager.generateRSAKeyPair();
+
+        FileHandler.saveKeyFile(keyPair.getPrivate(), PRIVATE_KEY_FILE);
+        FileHandler.saveKeyFile(keyPair.getPublic(), PUBLIC_KEY_FILE);
+
+        byte[] signature = RSAManager.rsaSign(Files.readAllBytes(MESSAGE_FILE_PATH), keyPair.getPrivate());
+        FileHandler.saveSignature(signature, SIGNATURE_FILE);
+        System.out.println(RSAManager.rsaVerify(Files.readAllBytes(MESSAGE_FILE_PATH), FileHandler.loadSignature(SIGNATURE_FILE), FileHandler.readPublicKey(PUBLIC_KEY_FILE)));
+        Thread.sleep(1000);
+        try {
+            KeyStore trustStore = KeyStore.getInstance("PKCS12");
+            try (FileInputStream trustStoreFile = new FileInputStream("client_truststore.p12")) {
+                trustStore.load(trustStoreFile, "password".toCharArray());
+            }
+
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("SunX509");
+            trustManagerFactory.init(trustStore);
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
+
+            SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+            try (SSLSocket socket = (SSLSocket) sslSocketFactory.createSocket(SERVER_ADDRESS,PORT);
+                 DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+                 DataInputStream input = new DataInputStream(socket.getInputStream())) {
+
+                output.writeInt(FILES_TO_SEND.length);
+
+                for (String filename : FILES_TO_SEND) {
+                    File file = new File(filename);
+                    if (!file.exists()) {
+                        System.out.println("File not found: " + filename);
+                        continue;
+                    }
+
+                    output.writeUTF(file.getName());
+                    output.writeLong(file.length());
+
+                    try (FileInputStream fileInputStream = new FileInputStream(file)) {
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                            output.write(buffer, 0, bytesRead);
+                        }
+                    }
+
+                    String response = input.readUTF();
+                    System.out.println("Server response: " + response);
+                }
+                output.writeUTF("END_TRANSFER");
+
+                System.out.println("All files sent successfully. Server shutdown signal sent.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
